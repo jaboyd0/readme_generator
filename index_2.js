7760bba70fb2aecb276e17a6239b9cfee2ddd1a3
Joshua Boyd <jaboyd0@gmail.com>
2020-04-11 23:09:11 -0400

    all questions added

--- index_2.js
+++ index_2.js
@@ -2,24 +2,25 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
 
+
 const writeFileAsync = util.promisify(fs.writeFile);
 
 function promptUser() {
   return inquirer.prompt([
-    {
+      {
         type: "input",
-        name: "Email",
-        message: "What is your Email address?"
+        name: "title",
+        message: "What is the Title of your project?"
       },
       {
         type: "input",
         name: "Description",
-        message: "What does your program do?"
+        message: "Give a brief description of your project."
       },
       {
         type: "input",
         name: "Table of Contents",
-        message: "What are the parts of your project?"
+        message: "Give a table of contents for your project"
       },
       {
         type: "input",
@@ -28,30 +29,45 @@ function promptUser() {
       },
       {
         type: "input",
+        name: "Usage",
+        message: "Give the usage of the project."
+      },
+      {
+        type: "input",
+        name: "License",
+        message: "What license will you need?"
+      },
+      {
+        type: "input",
+        name: "Contributing",
+        message: "Who contributed to the project?"
+      },
+      {
+        type: "input",
+        name: "Tests",
+        message: "what tests will be required?"
+      },
+      {
+        type: "input",
         name: "github",
         message: "Enter your GitHub Username"
       },
       {
         type: "input",
-        name: "linkedin",
-        message: "Enter your LinkedIn URL."
+        name: "Email",
+        message: "What is your Email address?"
+      },
+      {
+        type: "input",
+        name: "title",
+        message: "What is the Title of your project?"
       }
 ]);
 }
 
 function generateREADME(answers) {
   return `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-  <title>Document</title>
-</head>
-<body>
-  <div class="jumbotron jumbotron-fluid">
-  <div class="container">
+#${answers.title}
     <h1 class="display-4">Hi! My name is ${answers.name}</h1>
     <p class="lead">I am from ${answers.location}.</p>
     <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
@@ -70,7 +86,7 @@ function init() {
 
   promptUser()
     .then((answers) => {
-      return writeFileAsync("index.html", generateREADME(answers));
+      return writeFileAsync("newREADME.md", generateREADME(answers));
     }).then(() => {
       console.log("Successfully wrote to README");
     }).catch(error => console.log(error));
